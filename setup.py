c0edb8ccb7aeb4bd7a64695cab34fe1027e5a83c
Erick Monteiro <erick.m.anjos@gmail.com>
2023-10-31 11:37:47 -0300

    refatoring the code

--- setup.py
+++ setup.py
@@ -1,36 +1,38 @@
 import random
+import fake_data as fake
 
-# Criando a client_list
+# Creating client_list
 client_list = []
 
-# Randomizando os dados
-for i in range(100):
+# Randomizing data
+for i in range(150):
+    #first_name = random.choice(['Erick', 'Sarah', 'Fernanda', 'Renata', 'Fabio', 'Sheila', 'Renato', 'Victória', 'Gabriele', 'Mariana', 'Beatriz', 'Priscila', 'Gustavo', 'Bruno', 'Fabiano', 'Murilo', 'Gabriel', 'Pedro', 'Fagner', 'Luiz', 'João', 'Kleber', 'Rebeca', 'Victor', 'Lívia', 'Carol', 'Ana', 'Anna', 'Júlia', 'Heitor', 'Paulo', 'Letícia'])
+    #second_name = random.choice(['Monteiro', 'Alcaide', 'Fernandes', 'dos Anjos', 'Martins', 'de Souza', 'Souza', 'de Sousa', 'Sousa', 'Reis', 'Salim', 'Mendonça', 'José', 'Viviani', 'dos Santos', 'Rodrigues', 'Henrique', 'Potter', 'Brás', 'Araújo', 'Emanoel', 'Carolina', 'Carolyna', 'Carvalho', 'Medeiros', 'Marin', 'Vieira', 'Falcão', 'Silva', 'da Silva'])
+    #email = random.choice(['@gmail.com', '@outlook', '@hotmail.com', '@yahoo.com'])
 
-    primeiro_nome = random.choice(['Erick', 'Sarah', 'Fernanda', 'Renata', 'Fabio', 'Sheila', 'Renato', 'Victória', 'Gabriele', 'Mariana', 'Beatriz', 'Priscila', 'Gustavo', 'Bruno', 'Fabiano', 'Murilo', 'Gabriel', 'Pedro', 'Fagner', 'Luiz', 'João', 'Kleber', 'Rebeca', 'Victor', 'Lívia', 'Carol', 'Ana', 'Anna', 'Júlia', 'Heitor', 'Paulo', 'Letícia'])
-    segundo_nome = random.choice(['Monteiro', 'Alcaide', 'Fernandes', 'dos Anjos', 'Martins', 'de Souza', 'Souza', 'de Sousa', 'Sousa', 'Reis', 'Salim', 'Mendonça', 'José', 'Viviani', 'dos Santos', 'Rodrigues', 'Henrique', 'Potter', 'Brás', 'Araújo', 'Emanoel', 'Carolina', 'Carolyna', 'Carvalho', 'Medeiros', 'Marin', 'Vieira', 'Falcão', 'Silva', 'da Silva'])
-    email = random.choice(['@gmail.com', '@outlook', '@hotmail.com', '@yahoo.com'])
+    name = f'{random.choice(fake.first_name)} {random.choice(fake.second_name)}'
 
     # Dictionary
     client_dict = {
-        'nome': f'{primeiro_nome} {segundo_nome}', 
-        'contato': f'+55 (11) 9{random.randrange(1000, 9999, 1)}-{random.randrange(1000, 9999, 1)}',
-        'email': f'{primeiro_nome}.{segundo_nome.replace(" ", ".")}{email}'.lower(),
-        'endereco':f'R. blablabla N.{random.randrange(10, 99, 1)}', 
+        'name': name, 
+        'contact': f'+55 (11) 9{random.randrange(1000, 9999, 1)}-{random.randrange(1000, 9999, 1)}',
+        'email': f'{name.lower()}{random.choice(fake.email)}'.replace(" ", "."),
+        'address':f'R. blablabla N.{random.randrange(10, 99, 1)}', 
         'rg': f'{random.randrange(10, 99, 1)}.{random.randrange(100, 999, 1)}.{random.randrange(100, 999, 1)}-{random.randrange(1, 9, 1)}', 
         'cpf': f'{random.randrange(100, 999, 1)}.{random.randrange(100, 999, 1)}.{random.randrange(100, 999, 1)}/{random.randrange(10, 99, 1)}'
     }
  
-    # Aplicando cada dicionário de nome à client_list
+    # Adding every dictionary at the client_list
     client_list.append(client_dict)
 
-# Definindo o tamanho da client_list
+# Defining client_list lenght
 client_list_len = len(client_list)
 
-# Para cada nome na lisa ele imprime os dados
+# Printing the Client Data in a tabble
 for i in range(client_list_len):
-    print(f'Nome: {client_list[i]["nome"] :<20}|', end=' ')
-    print(f'Contato: {client_list[i]["contato"]} |', end=' ') 
-    print(f'E-Mail: {client_list[i]["email"] :<30} |', end=' ') 
-    print(f'Endereço: {client_list[i]["endereco"]} |', end=' ') 
+    print(f'Name: {client_list[i]["name"] :<20}|', end=' ')
+    print(f'Contact: {client_list[i]["contact"]} |', end=' ')
+    print(f'E-Mail: {client_list[i]["email"] :<30} |', end=' ')
+    print(f'Address: {client_list[i]["address"]} |', end=' ')
     print(f'RG: {client_list[i]["rg"]} |', end=' ')
     print(f'CPF: {client_list[i]["cpf"]}')
